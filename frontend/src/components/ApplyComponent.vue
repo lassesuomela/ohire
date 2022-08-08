6691a0a0cdbf79450435a8d700433025c07ec2cf
Lasse Suomela <suomelasse@gmail.com>
2022-08-09 01:00:21 +0300

    job application page is converted

--- frontend/src/components/ApplyComponent.vue
+++ frontend/src/components/ApplyComponent.vue
@@ -1,40 +1,45 @@
 <template>
-  <div class="field">
-    <h2>{{title}}</h2>
-  </div>
+<div class="container">
+  <el-card>
+    <el-main>
+      <el-form label-position="top">
+        <el-header>
+          <h2>{{title}}</h2>
+        </el-header>
+        <el-main>
+          <h2>Salary: {{salary}}</h2>
+        </el-main>
 
-  <div class="field">
-    <i class="material-symbols-outlined">edit_note</i>
-    <Textarea v-model="application" placeholder="Application" :autoResize="true" rows="8" cols="60" maxlength="1000"/>
-  </div>
-  <p id="application-help">{{application.length}} / 1000</p>
+        <el-form-item label="Job Application">
+          <el-input v-model="application" placeholder="Job Application" size="large" show-word-limit="true" maxlength="1000" type="textarea" :autosize="{minRows: 6}" class="customText"/>
+        </el-form-item>
 
-  <div class="field">
-    <Button label="Send" icon="pi pi-arrow-right" iconPos="right" @click="SendApplication"/>
-  </div>
-  
-  <Toast />
+        <div class="btn">
+          <el-button @click="SendApplication" size="large" type="primary">Send<i class="material-symbols-outlined">send</i></el-button>
+        </div>
+      </el-form>
+    </el-main>
+  </el-card>
+</div>
 </template>
 
 <script>
-import Textarea from 'primevue/textarea';
-import Button from 'primevue/button';
-import Toast from 'primevue/toast';
+import { ElButton, ElForm} from 'element-plus';
 
 import axios from '../axios';
 
 export default {
   name: 'LoginComponent',
   components: {
-    Button,
-    Textarea,
-    Toast,
+    ElButton,
+    ElForm
   },
   data () {
     return {
       application: '',
       status: null,
       title: null,
+      salary: null,
       jobId: this.$route.params.id,
     }
   },
@@ -42,7 +47,7 @@ export default {
     SendApplication () {
 
       if(!this.application){
-        this.$toast.add({severity:'warn', summary: 'One or more fields must be provided', detail:'', life: 3000});
+        this.$notify({title:"Warning", message:"One or more fields must be provided", type:"warning", customClass:"notification"});
         return;
       }
 
@@ -57,15 +62,17 @@ export default {
         if(response.data.status === "success"){
           this.status = response.data.message;
 
-          // send success toast msg
-          this.$toast.add({severity:'success', summary: this.status, life: 2000});
+          // send success msg
+          this.$notify({title:"Success", message:this.status, type:"success", customClass:"notification"});
+
+          this.$router.push('/jobs');
         }
 
         if(response.data.status === "error"){
           this.status = response.data.message;
 
-          // send error toast msg
-          this.$toast.add({severity:'error', summary: this.status, life: 2000});
+          // send error msg
+          this.$notify({title:"Error", message:this.status, type:"error", customClass:"notification"});
         }
 
       }).catch(error => {
@@ -78,19 +85,27 @@ export default {
 
         if(response.data.status === "success"){
           this.title = response.data.data.title;
+          this.salary = this.FormatSalary(response.data.data.salary);
         }
 
         if(response.data.status === "error"){
           this.status = response.data.message;
 
-          // send error toast msg
-          this.$toast.add({severity:'error', summary: this.status, life: 2000});
+          // send error msg
+          this.$notify({title:"Error", message:this.status, type:"error", customClass:"notification"});
         }
 
       }).catch(error => {
         console.log(error);
       })
-    }
+    },
+    FormatSalary(salary){
+
+      return new Intl.NumberFormat('fi', {
+        style: 'currency',
+        currency: 'EUR'
+      }).format(salary);
+    },
   },
   mounted () {
     this.FetchJobData(this.jobId);
@@ -100,21 +115,23 @@ export default {
 
 <style scoped>
 
-.field i {
-  font-size: 2rem;
-  padding-right: 1rem;
-}
-
-.field {
+.container {
   justify-content: center;
   text-align: center;
-  display: flex;
-  padding: 0.5rem;
+  display: grid;
+  font-size: 1.2em;
 }
 
-#application-help {
-  padding-top: 0rem;
-  padding-left: 30rem;
+.btn{
+  padding-top: 2rem;
 }
 
+.btn i{
+  padding-left: 0.5rem;
+}
+
+.customText {
+  color: black;
+  font-size: 16px;
+}
 </style>
