c44e8d59f6d90b6cbd1c804656e66329c64dd256
Lasse Suomela <suomelasse@gmail.com>
2022-08-11 15:40:09 +0300

    added order by timestamp desc, when fetching meetings

--- backend/models/meetingsModel.js
+++ backend/models/meetingsModel.js
@@ -5,7 +5,7 @@ const applications = {
         return db.query("INSERT INTO meetings (timestamp, description) VALUES (?, ?); INSERT INTO participants (meetings_id, users_id) VALUES (?, ?)", [data.timestamp, data.description, data.meetings_id, data.users_id], cb);
     },
     getByUsersId: (id, cb) => {
-        return db.query("SELECT users.username, users.uuid, meetings.id as 'meetingId', meetings.timestamp, meetings.description FROM participants p1 inner join participants p2 on p2.meetings_id = p1.meetings_id inner join users on users.id = p2.users_id inner join meetings on meetings.id = p2.meetings_id WHERE p1.users_id = ? AND p2.users_id <> ?", [id, id], cb);
+        return db.query("SELECT users.username, users.uuid, meetings.id as 'meetingId', meetings.timestamp, meetings.description FROM participants p1 inner join participants p2 on p2.meetings_id = p1.meetings_id inner join users on users.id = p2.users_id inner join meetings on meetings.id = p2.meetings_id WHERE p1.users_id = ? AND p2.users_id <> ? ORDER BY meetings.timestamp DESC", [id, id], cb);
     }
 }
 
