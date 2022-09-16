564f9a413180cc8ccf264d5d99e083540cacc1ea
Lasse Suomela <suomelasse@gmail.com>
2022-09-16 23:57:21 +0300

    added fqdn to axios baseurl and enabled ssl in nginx, added correct paths to ssl keys

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081/api";
+axios.defaults.baseURL = "https://jobs.lassesuomela.com/api";
 
 axios.defaults.headers.common["authorization"] = "Bearer " + localStorage.getItem("token");
 
