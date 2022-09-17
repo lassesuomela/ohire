449857a129f69303764744c010033ce0ccb0c832
Lasse Suomela <suomelasse@gmail.com>
2022-09-17 14:17:38 +0300

    added more recent db file

--- backend/mysql-docker/db.sql
+++ backend/mysql-docker/db.sql
@@ -1,142 +1,145 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ohire
+-- ------------------------------------------------------
+-- Server version	8.0.23
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema ohire
--- -----------------------------------------------------
+--
+-- Current Database: `ohire`
+--
 
--- -----------------------------------------------------
--- Schema ohire
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ohire` DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci ;
-USE `ohire` ;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ohire` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
--- -----------------------------------------------------
--- Table `ohire`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ohire`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(60) NOT NULL,
-  `password` VARCHAR(60) NOT NULL,
-  `email` VARCHAR(60) NOT NULL,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `role` TINYINT(1) NOT NULL DEFAULT '1',
-  `uuid` VARCHAR(255) NULL DEFAULT NULL,
-  `company` VARCHAR(255) NULL DEFAULT NULL,
+USE `ohire`;
+
+--
+-- Table structure for table `applications`
+--
+
+DROP TABLE IF EXISTS `applications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `applications` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `application` varchar(1000) COLLATE utf8_swedish_ci NOT NULL,
+  `cvFile` varchar(200) COLLATE utf8_swedish_ci NOT NULL,
+  `applicationFile` varchar(200) COLLATE utf8_swedish_ci NOT NULL,
+  `rating` tinyint DEFAULT NULL,
+  `users_id` int NOT NULL,
+  `joblistings_id` int NOT NULL,
+  `reviewed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC) VISIBLE,
-  UNIQUE INDEX `company_UNIQUE` (`company` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 46
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_swedish_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_jobListings_users_idx` (`users_id`),
+  KEY `fk_applications_joblistings1_idx` (`joblistings_id`),
+  CONSTRAINT `fk_applications_joblistings1` FOREIGN KEY (`joblistings_id`) REFERENCES `joblistings` (`id`),
+  CONSTRAINT `fk_jobListings_users0` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
--- -----------------------------------------------------
--- Table `ohire`.`joblistings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ohire`.`joblistings` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `description` VARCHAR(1000) NULL DEFAULT NULL,
-  `title` VARCHAR(60) NULL DEFAULT NULL,
-  `salary` INT NULL DEFAULT NULL,
-  `users_id` INT NOT NULL,
-  `workingTime` TINYINT(1) NULL DEFAULT '1',
+DROP TABLE IF EXISTS `joblistings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `joblistings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` varchar(1000) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `title` varchar(60) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `salary` int DEFAULT NULL,
+  `users_id` int NOT NULL,
+  `workingTime` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_jobListings_users_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_jobListings_users`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `ohire`.`users` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 31
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_swedish_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_jobListings_users_idx` (`users_id`),
+  CONSTRAINT `fk_jobListings_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `meetings`
+--
 
--- -----------------------------------------------------
--- Table `ohire`.`applications`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ohire`.`applications` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `application` VARCHAR(1000) NOT NULL,
-  `cvFile` VARCHAR(200) NOT NULL,
-  `applicationFile` VARCHAR(200) NOT NULL,
-  `rating` TINYINT NULL DEFAULT NULL,
-  `users_id` INT NOT NULL,
-  `joblistings_id` INT NOT NULL,
-  `reviewed` TINYINT(1) NOT NULL DEFAULT '0',
+DROP TABLE IF EXISTS `meetings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meetings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL,
+  `description` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `expired` tinyint(1) DEFAULT '0',
+  `joblistings_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_jobListings_users_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_applications_joblistings1_idx` (`joblistings_id` ASC) VISIBLE,
-  CONSTRAINT `fk_applications_joblistings1`
-    FOREIGN KEY (`joblistings_id`)
-    REFERENCES `ohire`.`joblistings` (`id`),
-  CONSTRAINT `fk_jobListings_users0`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `ohire`.`users` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 24
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_swedish_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_meetings_joblistings1_idx` (`joblistings_id`),
+  CONSTRAINT `fk_meetings_joblistings1` FOREIGN KEY (`joblistings_id`) REFERENCES `joblistings` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+-- Table structure for table `participants`
+--
 
--- -----------------------------------------------------
--- Table `ohire`.`meetings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ohire`.`meetings` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `timestamp` TIMESTAMP NOT NULL,
-  `description` VARCHAR(255) NULL DEFAULT NULL,
-  `expired` TINYINT(1) NULL DEFAULT '0',
-  `joblistings_id` INT NOT NULL,
+DROP TABLE IF EXISTS `participants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `participants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `meetings_id` int NOT NULL,
+  `users_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_meetings_joblistings1_idx` (`joblistings_id` ASC) VISIBLE,
-  CONSTRAINT `fk_meetings_joblistings1`
-    FOREIGN KEY (`joblistings_id`)
-    REFERENCES `ohire`.`joblistings` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_swedish_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_participants_meetings1_idx` (`meetings_id`),
+  KEY `fk_participants_users1_idx` (`users_id`),
+  CONSTRAINT `fk_participants_meetings1` FOREIGN KEY (`meetings_id`) REFERENCES `meetings` (`id`),
+  CONSTRAINT `fk_participants_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `users`
+--
 
--- -----------------------------------------------------
--- Table `ohire`.`participants`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ohire`.`participants` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `meetings_id` INT NOT NULL,
-  `users_id` INT NOT NULL,
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) COLLATE utf8_swedish_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_swedish_ci NOT NULL,
+  `email` varchar(60) COLLATE utf8_swedish_ci NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` tinyint(1) NOT NULL DEFAULT '1',
+  `uuid` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `company` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `profilePic` varchar(255) COLLATE utf8_swedish_ci DEFAULT 'default.png',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_participants_meetings1_idx` (`meetings_id` ASC) VISIBLE,
-  INDEX `fk_participants_users1_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_participants_meetings1`
-    FOREIGN KEY (`meetings_id`)
-    REFERENCES `ohire`.`meetings` (`id`),
-  CONSTRAINT `fk_participants_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `ohire`.`users` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 8
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_swedish_ci;
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
+  UNIQUE KEY `company_UNIQUE` (`company`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-17 14:14:50
