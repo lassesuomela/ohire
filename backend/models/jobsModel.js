be56476ad07ede431669d97e6995f4e9eee05669
Lasse Suomela <suomelasse@gmail.com>
2022-09-17 12:25:15 +0300

    changed jobListings to joblistings that somehow changed at some point which couses sql queries to fail

--- backend/models/jobsModel.js
+++ backend/models/jobsModel.js
@@ -2,19 +2,19 @@ const db = require('../configs/db');
 
 const jobs = {
     add: (data, cb) =>{
-        return db.query('INSERT INTO jobListings (title, description, users_id, salary, workingTime) VALUES (?, ?, ?, ?, ?)', [data.title, data.description, data.users_id, data.salary, data.workingTime], cb)
+        return db.query('INSERT INTO joblistings (title, description, users_id, salary, workingTime) VALUES (?, ?, ?, ?, ?)', [data.title, data.description, data.users_id, data.salary, data.workingTime], cb)
     },
     getCount: (filter, cb) => {
-        return db.query("SELECT COUNT(id) AS maxCount FROM jobListings WHERE jobListings.title LIKE CONCAT('%', ?, '%')", [filter], cb);
+        return db.query("SELECT COUNT(id) AS maxCount FROM joblistings WHERE joblistings.title LIKE CONCAT('%', ?, '%')", [filter], cb);
     },
     getById: (id, cb) => {
-        return db.query('SELECT * from jobListings WHERE id = ? LIMIT 1', [id], cb);
+        return db.query('SELECT * from joblistings WHERE id = ? LIMIT 1', [id], cb);
     },
     getNAmountOfPostings: (filter, n, cb) =>{
-        return db.query("SELECT jobListings.id, jobListings.timestamp, jobListings.description, jobListings.title, jobListings.salary, jobListings.workingTime, users.company FROM jobListings JOIN users ON jobListings.users_id = users.id WHERE jobListings.title LIKE CONCAT('%', ?, '%') ORDER BY id DESC LIMIT ?, 10", [filter, n], cb);
+        return db.query("SELECT joblistings.id, joblistings.timestamp, joblistings.description, joblistings.title, joblistings.salary, joblistings.workingTime, users.company FROM joblistings JOIN users ON joblistings.users_id = users.id WHERE joblistings.title LIKE CONCAT('%', ?, '%') ORDER BY id DESC LIMIT ?, 10", [filter, n], cb);
     },
     deleteById: (users_id, id, cb) => {
-        return db.query('DELETE FROM jobListings WHERE users_id = ? AND id = ?', [users_id, id], cb);
+        return db.query('DELETE FROM joblistings WHERE users_id = ? AND id = ?', [users_id, id], cb);
     },
     getCompanysPostingCount: (id, cb) => {
         return db.query("SELECT COUNT(id) AS maxCount FROM joblistings WHERE users_id = ?", [id], cb);
